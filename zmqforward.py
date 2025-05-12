e40bfb895ca7aeb931b69e1738c46e94c449ac05
Aaron Oostdijk <aoostdijk@gmail.com>
2025-05-12 14:37:24 +0200

    Correct declare order

--- zmqforward.py
+++ zmqforward.py
@@ -13,21 +13,6 @@ localClient = udp_client.UDPClient("127.0.0.1", 8000)
 poller = zmq.Poller()
 poller.register(socket, zmq.POLLIN)
 
-# Loop and accept messages from both channels, acting accordingly
-while True:
-    # TODO: Might need to retry connection?
-    socks = dict(poller.poll(1000))
-    if socks:
-        if socks.get(socket) == zmq.POLLIN:
-            dgram = socket.recv(zmq.NOBLOCK)
-            # Untested convert to osc message (from dgram)
-            localClient.send(osc_message.OscMessage(dgram))
-            # print("got message ",message)
-    else:
-        # testMessage()
-        # print("error: message timeout")
-        time.sleep(0.1)
-
 def testMessage():
     # mesh template tester
     msg = osc_message_builder.OscMessageBuilder(address="/object/MeshTemplate")
@@ -61,3 +46,19 @@ def testMessage():
     
     builtMessage = msg.build()
     localClient.send(builtMessage)
+
+# Loop and accept messages from both channels, acting accordingly
+while True:
+    # TODO: Might need to retry connection?
+    socks = dict(poller.poll(1000))
+    if socks:
+        if socks.get(socket) == zmq.POLLIN:
+            dgram = socket.recv(zmq.NOBLOCK)
+            # Untested convert to osc message (from dgram)
+            localClient.send(osc_message.OscMessage(dgram))
+            # print("got message ",message)
+    else:
+        # testMessage()
+        # print("error: message timeout")
+        time.sleep(0.1)
+
